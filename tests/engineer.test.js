cf0ce43286c80ff632b157722e2370eec39a4688
Ryan Street <ryanstreet122@gmail.com>
2022-11-21 12:46:32 -0500

    tests running successfully

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -4,22 +4,22 @@ const Engineer = require('../lib/engineer');
 describe('engineer',()=>{
 
     describe("initialization",()=>{
-    it('should create an engineer object');
+    it('should create an engineer object',()=>{
     const engineer = new Engineer('Ryan', 25, "ryanstreet122@gmail.com", 'RyStreet');
     expect(engineer.github).toEqual('RyStreet')
-    });
+    })});
 
     describe('getGitHub()',()=>{
-        it('should grab the github profile of employee')
+        it('should grab the github profile of employee', ()=>{
         const engineer = new Engineer('Ryan', 25, "ryanstreet122@gmail.com", 'RyStreet')
         expect(engineer.getGithub()).toEqual('RyStreet');
         
-    })
+    })})
 
     describe('getRole()',()=>{
-        it('should override role to engineer')
+        it('should override role to engineer', ()=>{
         const engineer = new Engineer('Ryan', 25, "ryanstreet122@gmail.com", 'RyStreet')
         expect(engineer.getRole()).toEqual('Engineer');
 
-    })
+    })})
 })
